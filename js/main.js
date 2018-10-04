e4f809f2a85c6d74e7f69cf892bf70471aacff3d
Unknown <bilyksaha01@gmail.com>
2018-10-04 14:30:04 +0300

    Доделал слайдер

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$('.why_us-slider').slick({
 	    infinite: true,
-	    slidesToShow: 2,
+	    slidesToShow: 4,
 	    slidesToScroll: 2,
 	    arrows: true,
 	    prevArrow: '<i class="fa why_us-slider-arrows arrow-left fa-chevron-left"></i>',
@@ -9,15 +9,22 @@ $(document).ready(function(){
 	    dots: false,
 	    responsive: [
 	    {
-	      breakpoint: 1024,
+	      breakpoint: 992,
 	      settings: {
-	        slidesToShow: 1,
+	        slidesToShow: 3,
 	        slidesToScroll: 1,
 	        infinite: true,
 	      }
 	    },
 	    {
-	      breakpoint: 320,
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 480,
 	      settings: {
 	        slidesToShow: 1,
 	        slidesToScroll: 1
