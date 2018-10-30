4f9ac83632eabf57d5538693989e8c3e25f91e0e
Unknown <bilyksaha01@gmail.com>
2018-10-30 16:58:02 +0200

    Сделал страницу mix&match.html, адаптировал её.

--- js/main.js
+++ js/main.js
@@ -36,9 +36,6 @@ $(document).ready(function(){
 	        slidesToScroll: 1
 	      }
 	    }
-	    // You can unslick at a given breakpoint now by adding:
-	    // settings: "unslick"
-	    // instead of a settings object
 	  	]
 	});
 
@@ -83,13 +80,47 @@ $(document).ready(function(){
 	        slidesToScroll: 1
 	      }
 	    }
-	    // You can unslick at a given breakpoint now by adding:
-	    // settings: "unslick"
-	    // instead of a settings object
 	  	]
 	});
 
 	/*Start settings for slider in the section Process*/
+
+	/*Start settings for slider in the section Features*/
+
+	$('.features__slider').slick({
+	    infinite: true,
+	    slidesToShow: 4,
+	    slidesToScroll: 2,
+	    arrows: true,
+	    prevArrow: '<i class="fa slider-arrows arrow-left features__slider-arrow-left fa-chevron-left"></i>',
+	    nextArrow: '<i class="fa slider-arrows arrow-right features__slider-arrow-right fa-chevron-right"></i>',
+	    dots: false,
+	    responsive: [
+	    {
+	      breakpoint: 992,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 575,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	  	]
+	});
+
+	/*Start settings for slider in the section Features*/
 
 	/*Finish settings for sliders*/
 	
